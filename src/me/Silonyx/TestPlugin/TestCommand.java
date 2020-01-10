c233e96d7dd26b087311e9b06e9a728889454e13
silonyx <59715488+silonyx@users.noreply.github.com>
2020-01-10 16:58:32 -0500

    creeper teleport

--- src/me/Silonyx/TestPlugin/TestCommand.java
+++ src/me/Silonyx/TestPlugin/TestCommand.java
@@ -1,12 +1,16 @@
 package me.Silonyx.TestPlugin;
 
-import java.util.HashSet;
-
-import org.bukkit.Material;
+import java.util.ArrayList;
+import java.util.Queue;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Mob;
+import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 
 public class TestCommand implements CommandExecutor {
@@ -15,10 +19,23 @@ public class TestCommand implements CommandExecutor {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             
-            HashSet<Material> transparent = new HashSet<Material>();
-            transparent.add(Material.AIR);
-            Block block = player.getTargetBlock(transparent, 250);
-            player.getWorld().strikeLightning(block.getLocation());
+            
+            ArrayList q = (ArrayList) player.getWorld().getPlayers();
+            Player p2 = null;
+            
+            for(int i=0; i<q.size(); i++) {
+            	
+	            if(q.get(i).equals(player)) {
+	            }
+	            else {
+	            	p2 =(Player)(q.get(i));
+	            }
+            }
+	            
+            Block block = player.getTargetBlock(null, 250);
+            Location loc = block.getLocation();
+            p2.teleport(loc);
+            player.getWorld().spawnEntity(loc, EntityType.CREEPER);
             //BIG CHANGES TO BE MADE
             
             return true;
